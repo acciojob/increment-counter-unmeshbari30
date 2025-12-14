ba8990681c3b6f8b827fde2288e3697ece2af0dc
Unmesh Bari <unmeshbari5@gmail.com>
2025-12-14 22:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+	//your JS code here. If required.
+	let counter = 0
+	let body = document.querySelector(".my_body")
+	let btn = document.getElementById('incrementBtn')
+
+	let element = document.createElement('p')
+	element.textContent = counter
+	body.appendChild(element)
+	
+	btn.addEventListener('click', ()=>{
+		alert(counter)
+		element.textContent = counter;
+		counter++
+	})
